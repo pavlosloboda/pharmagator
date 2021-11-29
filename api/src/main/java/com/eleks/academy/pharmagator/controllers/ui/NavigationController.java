61fdde1e010eeb831a5f02f634484f2c2de58bfc
TOlya <toly.artmchk@gmail.com>
2021-11-29 08:06:24 +0200

    feat: add ability to show prises

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/ui/NavigationController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/ui/NavigationController.java
@@ -1,18 +1,27 @@
 package com.eleks.academy.pharmagator.controllers.ui;
 
+import com.eleks.academy.pharmagator.entities.Pharmacy;
+import com.eleks.academy.pharmagator.services.ExportService;
 import com.eleks.academy.pharmagator.services.MedicineService;
+import com.eleks.academy.pharmagator.services.PharmacyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/ui")
 public class NavigationController {
 
     private final MedicineService medicineService;
+    private final PharmacyService pharmacyService;
+    private final ExportService exportService;
 
-    @RequestMapping({"", "/", "index", "index.html", "home", "homepage"})
+    @RequestMapping({"", "/", "/index", "/index.html", "/home", "/homepage"})
     public String getHomePage() {
         return "index";
     }
@@ -24,7 +33,12 @@ public class NavigationController {
     }
 
     @RequestMapping("/price")
-    public String getPrices() {
+    public String getPrices(Model model) {
+        List<Pharmacy> pharmacies = pharmacyService.findAll();
+        HashMap<Long, String> pharmaciesWithId = new HashMap<>(pharmacies.size());
+
+        model.addAttribute("pricesMap", exportService.getMapPrices());
+        model.addAttribute("pharmacies", pharmacies);
         return "price";
     }
 
